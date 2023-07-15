3c0dea60e4eb7b78fa03c1f9b6bae5cd1388428e
Hoi Won KIM <gw31513@gmail.com>
2023-07-15 23:03:24 +1200

    APP

---
+++ Documents/kidsgram-frontend/src/App.tsx
@@ -0,0 +1,28 @@
+// ./src/App.tsx
+import React from 'react';
+import Router from "./Router";
+import GlobalStyle from "./styles/GlobalStyle";
+import client, { isDarkModeVar } from "./apollo";
+import { ThemeProvider } from "styled-components";
+import { HelmetProvider } from "react-helmet-async";
+import { darkTheme, lightTheme } from "./styles/themes";
+import { ApolloProvider, useReactiveVar } from "@apollo/client";
+
+const App: React.FC = () => {
+  const isDarkMode: boolean = useReactiveVar(isDarkModeVar);
+
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <HelmetProvider>
+          <ThemeProvider theme={isDarkMode === true ? darkTheme : lightTheme}>
+            <GlobalStyle />
+            <Router />
+          </ThemeProvider>
+        </HelmetProvider>
+      </ApolloProvider>
+    </>
+  );
+};
+
+export default App;
